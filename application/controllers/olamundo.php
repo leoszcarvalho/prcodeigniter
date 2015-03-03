17600912dc47c663318dd307f0a37545c8d2ee63
leoszcarvalho <leonardo.souzas30@gmail.com>
2015-03-03 20:28:37 -0300

    Commit passagem de variaveis

---
+++ application/controllers/olamundo.php
@@ -0,0 +1,37 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Olamundo extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('ola');
+	}
+        
+        public function cadastrar()
+	{
+                $array = array('primeira' => 'um',
+                    'segunda' => 'dois',
+                    'array' => array('first', 'second'),
+                    'segmento' => $this->uri->segment(3));
+		$this->load->view('cadastrar',$array);
+	}
+        
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/cadastrar.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Cadastrar</title>
+</head>
+<body>
+
+<p><?php echo $primeira."-".$segunda;?></p>
+<br>
+<?php
+foreach($array as $item)
+{
+    echo $item."-";
+}
+?>
+
+<br>
+
+<?php
+echo "Parametro: $segmento";
+?>
+
+</body>
+</html>
